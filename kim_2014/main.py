f87498f52ab5f2e38d187b41ee9b9f3adba096c9
leonardtang <leonardgentwintang@gmail.com>
2020-07-24 10:49:13 -0400

    cuda out of memory

--- kim_2014/main.py
+++ kim_2014/main.py
@@ -29,7 +29,7 @@ random.seed(SEED)
 torch.backends.cudnn.deterministic = True
 
 # Loading device
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
 
